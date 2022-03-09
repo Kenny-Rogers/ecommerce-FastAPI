b8b075e65959d5657d53e108185ef1eb933cc257
Kennedy Yaw Lodonu <kennedy@hubtel.com>
2022-03-09 02:07:46 +0000

    6. Writing RestAPI 2

--- ecommerce/user/services.py
+++ ecommerce/user/services.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from fastapi import HTTPException, status
 from . import models
 from . import schema
@@ -9,3 +11,22 @@ async def new_user_register(request: schema.User, database) -> models.User:
     database.commit()
     database.refresh(new_user)
     return new_user
+
+
+async def all_users(database) -> List[models.User]:
+    users = database.query(models.User).all()
+    return users
+
+
+async def get_user_by_id(user_id, database) -> Optional[models.User]:
+    user_info = database.query(models.User).get(user_id)
+
+    if not user_info:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data Not Found !")
+
+    return user_info
+
+
+async def delete_user_by_id(user_id, database):
+    database.query(models.User).filter(models.User.id == user_id).delete()
+    database.commit()
